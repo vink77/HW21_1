7a7e18d144e9784db9626c116959fa4a61519f7f
vink77 <vink.av@mail.ru>
2023-09-12 05:27:45 +0700

    add settings db

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DB_magazin',
+        'USER': 'postgres',
+        'PASSWORD': '9877'
     }
 }
 
