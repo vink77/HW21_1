e09507b5193de1202e43ce55e0649c7af4b060fa
vink77 <vink.av@mail.ru>
2023-09-17 23:18:17 +0700

    созданы карточки

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from django.contrib import admin
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, info1
+from catalog.views import index, contacts, info1, ProductCreateView
 from django.conf.urls.static import static
 from django.conf import settings
 
 app_name = CatalogConfig.name
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contacts/', contacts, name='contact'),
     path('info1/', info1, name='info1'),
-
+    path('create/', ProductCreateView.as_view(), name='create_product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from catalog.models import Category, Product
+from django.urls import reverse_lazy
 
+from catalog.models import Category, Product
+from django.views.generic import CreateView, ListView
 
 
 # Create your views here.
@@ -18,3 +20,11 @@ def contacts(request):
 
 def info1(request):
     return render(request,'catalog/info1.html')
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('product_name', 'product_description', 'price','date_create','date_last_change')
+    success_url = reverse_lazy('catalog:create_product')
+
+class ProductListView(ListView):
+    model = Product
