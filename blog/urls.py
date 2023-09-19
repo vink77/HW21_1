eaa4c02a64817cfe5ac8ad3a656dc6925132d913
Vink77 <vink.av@mail.ru>
2023-09-19 17:19:21 +0700

    preend version

--- blog/urls.py
+++ blog/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from blog.apps import BlogModelConfig
+from django.conf import settings
+
 from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
 from . import views
 
 app_name = BlogModelConfig.name
 
 urlpatterns = [
-    path('create/', BlogCreateView.as_view(), name='create'),
     path('', BlogListView.as_view(), name='list'),
-    path('detail/<pk>', BlogDetailView.as_view(), name='detail'),
-    path('update/<pk>', BlogUpdateView.as_view(), name='update'),
-    path('view/<pk>', BlogDetailView.as_view(), name='view'),
-    path('delete/<pk>', BlogDeleteView.as_view(), name='delete'),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('detail/<pk>/', BlogDetailView.as_view(), name='detail'),
+    path('update/<pk>/', BlogUpdateView.as_view(), name='update'),
+    path('delete/<pk>/', BlogDeleteView.as_view(), name='delete'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.urls import reverse_lazy
-
+from django.urls import reverse_lazy, reverse
 
 from blog.models import Blog
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
@@ -11,7 +10,7 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 
 # Create your views here.
 
-#def index(request):
+# def index(request):
 #    product_list = Product.objects.all()
 #    context = {
 #        'objects_list': product_list
@@ -22,35 +21,39 @@ from django.views.generic import CreateView, ListView, DetailView, UpdateView, D
 class BlogCreateView(CreateView):
     model = Blog
     fields = ('title', 'conteхt')
-    success_url = reverse_lazy('blog/blog_list.html')
+    success_url = reverse_lazy('blog:list')
+
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'conteхt')
-    success_url = reverse_lazy('blog/blog_list.html')
-    context_object_name = 'objects'
+    success_url = reverse_lazy('blog:list')
+    context_object_name = 'object_list'
+
+    def get_success_url(self):
+        return reverse('blog:detail', args=[self.kwargs.get('slug')])
 
 
 class BlogListView(ListView):
     model = Blog
-    template_name = 'blog/blog_list.html'
-    context_object_name = 'objects'
+    template_name = 'blog:blog_list'
+    context_object_name = 'object_list'
+
 
 class BlogDetailView(DetailView):
+    model = Blog
     template_name = 'blog/blog_detail.html'
-    context_object_name = 'object'
-    success_url = reverse_lazy('blog/blog_list.html')
-
+    context_object_name = 'object_list'
+    success_url = reverse_lazy('blog:list')
 
     def get_object(self, queryset=None):
-            self.object = super().get_object(queryset)
-            self.object.view_count +=1
-            self.object.save()
-            return self.object
-
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy('blog:list')
-    context_object_name = 'objects'
